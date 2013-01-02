eff46be1314eb63ccae14719622ba20b6d6bab8e
Alessio Bianchi <venator85@gmail.com>
2013-01-02 13:46:28 +0100

    fix bug

--- src/com/octo/android/robospice/persistence/springandroid/json/gson/GsonObjectPersister.java
+++ src/com/octo/android/robospice/persistence/springandroid/json/gson/GsonObjectPersister.java
@@ -127,11 +127,6 @@ public final class GsonObjectPersister<T> extends InFileObjectPersister<T> {
 		}
 	}
 
-	@Override
-	public boolean canHandleClass(Class<?> clazz) {
-		return true;
-	}
-
 	/** for testing purpose only. Overriding allows to regive package level visibility. */
 	@Override
 	protected void awaitForSaveAsyncTermination(long time, TimeUnit timeUnit) throws InterruptedException {
